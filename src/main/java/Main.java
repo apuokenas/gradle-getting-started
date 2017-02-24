a5c69b91df7b40a1c0ad7b9edc73371515f8af88
MT <mantas@tumenas.eu>
2017-02-24 17:25:41 +0200

    Add jscience

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,12 @@ import java.sql.*;
 
 import com.heroku.sdk.jdbc.DatabaseUrl;
 
+// Add jscience
+import static javax.measure.unit.SI.KILOGRAM;
+import javax.measure.quantity.Mass;
+import org.jscience.physics.model.RelativisticModel;
+import org.jscience.physics.amount.Amount;
+
 public class Main {
   public static void main(String... args) throws Exception {
     RatpackServer.start(s -> s
@@ -24,8 +30,11 @@ public class Main {
         .handlers(chain -> chain
             .get(ctx -> ctx.render(groovyTemplate("index.html")))
 
+            // Add jscience
             .get("hello", ctx -> {
-              ctx.render("Hello!");
+              RelativisticModel.select();
+              Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
+              ctx.render("E=mc^2: 12 GeV = " + m.toString());
             })
 
             .get("db", ctx -> {
